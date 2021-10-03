ecdd1a9f2f3aff9135bec00392eb95727d3ecd89
InmookJeong <jeonginmook@gmail.com>
2021-10-03 23:43:53 +0900

    feat(Application.java): 컴퓨터의 숫자 3개 가져오기
    - (기능1) 게임 시작 기능 구현
    - (기능2) 컴퓨터의 1~9 사이의 3자리 숫자 가져오기 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import nextstep.utils.Randoms;
+import utils.NumberUtil;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Application {
+
+    private static final Logger LOG = Logger.getLogger(Application.class.getName());
+
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        playBaseball();
+    }
+
+    // 숫자 야구 게임 시작(Start Point)
+    public static void playBaseball() {
+        // TODO 컴퓨터의 숫자
+        String computerNumbers = "";
+        while(computerNumbers.length() < 3) {
+            int randomNo = Randoms.pickNumberInRange(1, 9);
+            if(NumberUtil.isOneToNineNumeric(randomNo)) {
+                computerNumbers += String.valueOf(randomNo);
+            }
+        }
+        LOG.log(Level.INFO, "setComputerNumber is "+computerNumbers);
     }
 }
