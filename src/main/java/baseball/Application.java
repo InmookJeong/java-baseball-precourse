56a3f09eb8c1fd4e5bb9297c733c730e0d3f80c7
InmookJeong <jeonginmook@gmail.com>
2021-10-06 19:23:14 +0900

    feat(Application): 게임 재시작 기능 추가
    - 게임 재시작 메소드(restartBaseball) 추가
    - 재시작 할것인지에 대한 반환값(boolean type) 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,10 +18,14 @@ public class Application {
     }
 
     // 숫자 야구 게임 시작(Start Point)
-    public static void playBaseball() {
+    public static boolean playBaseball() {
         String computerNumbers = setComputerNumbers();
-        System.out.println("computerNumbers : " + computerNumbers);
-        checkResultByUserInput(computerNumbers);
+        boolean isGameOver = false;
+        while(!isGameOver) {
+            isGameOver = checkResultByUserInput(computerNumbers);
+        }
+        // return isGameOver;
+        return restartBaseball();
     }
 
     // 컴퓨터의 숫자 세팅
@@ -44,15 +48,14 @@ public class Application {
     }
 
     // 사용자가 값을 입력한 후 결과 확인하기
-    public static void checkResultByUserInput(String computerNumbers) {
+    public static boolean checkResultByUserInput(String computerNumbers) {
         // 사용자로부터 숫자 입력 받기
         String userInuptNumbers = getInputUserNumbers();
         if(isNothing(computerNumbers, userInuptNumbers)) {
             System.out.println("낫싱");
-            checkResultByUserInput(computerNumbers);
-            return;
+            return false;
         }
-        viewResult(computerNumbers, userInuptNumbers);
+        return viewResult(computerNumbers, userInuptNumbers);
     }
     
     // 사용자 입력 숫자 가져오기
@@ -80,10 +83,19 @@ public class Application {
     }
     
     // 사용자의 입력값이 컴퓨터와 얼마나 일치하는지 확인
-    public static void viewResult(String computerNo, String userInputNo) {
+    public static boolean viewResult(String computerNo, String userInputNo) {
         BaseballCount baseballCount = new BaseballCount(computerNo, userInputNo);
-        if(!baseballCount.isGameOver()) {
-            checkResultByUserInput(computerNo);
+        return baseballCount.isGameOver();
+    }
+
+    // 게임을 재시작할 것인지 확인
+    public static boolean restartBaseball() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String restart = Console.readLine();
+        if(restart.equals("1")) {
+            playBaseball();
         }
+        return false;
     }
 }
